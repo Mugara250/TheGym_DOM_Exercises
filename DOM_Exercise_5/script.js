a7d895f49039ed539df6741b432f029cde2dc5e8
Mugara250 <mbonyumugara52@gmail.com>
2024-12-31 22:15:51 +0200

    dom exercises

---
+++ DOM_Exercise_5/script.js
@@ -0,0 +1,16 @@
+function jsStyle() {
+    const paragraph = document.querySelector("#text");
+    paragraph.style.fontSize = "2rem";
+    paragraph.style.color = "green";
+    paragraph.style.fontFamily = "Verdana, Geneva, Tahoma, sans-serif"
+}
+
+// const button = document.querySelector("#jsstyle");
+
+// button.addEventListener('click', function changeStyle(event) {
+//     const paragraph = document.querySelector("#text");
+//     // console.log(event.target)
+//     paragraph.style.fontFamily = "Verdana, Geneva, Tahoma, sans-serif";
+//     paragraph.style.fontSize = "2rem";
+//     paragraph.style.color = "green";
+// })
