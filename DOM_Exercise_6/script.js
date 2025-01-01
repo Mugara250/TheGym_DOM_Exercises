915e354da273772a46c4eeb9273f6c0f3323213c
Mugara250 <mbonyumugara52@gmail.com>
2025-01-01 18:34:50 +0200

    DOM_Exercise_6

---
+++ DOM_Exercise_6/script.js
@@ -0,0 +1,8 @@
+function getFormValue(event) {
+  event.preventDefault();
+  const firstName = document.getElementsByName("fname")[0].value;
+  console.log(firstName);
+
+  const lastName = document.getElementsByName("lname")[0].value;
+  console.log(lastName);
+}
