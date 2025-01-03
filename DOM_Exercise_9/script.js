3ce702b883b292502447175c0d49f1217bac88f9
Mugara250 <mbonyumugara52@gmail.com>
2025-01-03 15:03:04 +0200

    DOM_Exercise_9

--- DOM_Exercise_9/script.js
+++ DOM_Exercise_9/script.js
@@ -4,10 +4,11 @@ const removeTextButton = document.querySelector("#remove-text-btn");
 const textArea = document.querySelector("textarea");
 
 // adding event listeners to our buttons
-setTextButton.addEventListener("click", function () {
+setTextButton.addEventListener("click", function setText() {
   textArea.style.fontSize = "1rem";
   textArea.textContent =
     "I have set the text inside the area. It is really cool to learn how this is done.";
+  setTextButton.removeEventListener("click", setText);
 });
 
 removeTextButton.addEventListener("click", function () {
